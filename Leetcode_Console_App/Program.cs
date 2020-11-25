fbaff3219ac585b170d193981e7828b9c771df2b
deadislove <taweilin7689@gmail.com>
2020-11-25 09:31:55 +0800

    test

--- Leetcode_Console_App/Program.cs
+++ Leetcode_Console_App/Program.cs
@@ -10,8 +10,7 @@ namespace Leetcode_Console_App
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
