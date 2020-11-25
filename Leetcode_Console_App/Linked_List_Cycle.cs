d9378de47576120be2da2ce8da47b297e9a79d40
deadislove <taweilin7689@gmail.com>
2020-11-25 14:09:05 +0800

    Update

---
+++ Leetcode_Console_App/Linked_List_Cycle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Console_App
+{
+    class Linked_List_Cycle
+    {
+        //Problem link: https://leetcode.com/problems/linked-list-cycle/
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x)
+            {
+                val = x;
+                next = null;
+            }
+        }
+
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> s = new HashSet<ListNode>();
+
+            while (head != null)
+            {
+                // If we have already has this node 
+                // in hashmap it means their is a cycle 
+                // (Because you we encountering the 
+                // node second time). 
+                if (s.Contains(head))
+                    return true;
+
+                // If we are seeing the node for 
+                // the first time, insert it in hash 
+                s.Add(head);
+
+                head = head.next;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Leetcode_Console_App/Min_Stack.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode_Console_App
+{
+    class Min_Stack
+    {
+        //Problem link: https://leetcode.com/problems/min-stack/
+        /* Reference
+         * GeeksForgeeks solution
+         * Link: https://www.geeksforgeeks.org/design-a-stack-that-supports-getmin-in-o1-time-and-o1-extra-space/
+         */
+
+        public class MinStack
+        {
+            public Stack s;
+            public int minEle;
+            /** initialize your data structure here. */
+            public MinStack()
+            {
+                s = new Stack();
+            }
+
+            public void Push(int x)
+            {
+                if (s.Count == 0)
+                {
+                    minEle = x;
+                    s.Push(x);
+                    return;
+                }
+
+                if (x < minEle)
+                {
+                    s.Push(2 * x - minEle);
+                    minEle = x;
+                }
+                else
+                    s.Push(x);
+            }
+
+            public void Pop()
+            {
+                if (s.Count == 0)
+                    return;
+                int t = (int)s.Pop();
+
+                if(t<minEle)
+                    minEle = 2 * minEle - t;
+            }
+
+            public int Top()
+            {
+                return(int)s.Peek();
+            }
+
+            public int GetMin()
+            {
+                if (s.Count == 0)
+                    return 0;
+                else
+                    return minEle;
+            }
+        }
+    }
+}
