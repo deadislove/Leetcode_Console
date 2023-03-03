f5306fd937c343974ffc36aa5c51d9b557c169c6
deadislove <taweilin7689@gmail.com>
2023-03-03 16:43:00 +0800

    add 4 cases. 2023/3/3

--- Leetcode_Console_App/4Sum.cs
+++ Leetcode_Console_App/4Sum.cs
@@ -7,57 +7,74 @@ namespace Leetcode_Console_App
     {
         public static void Client() { }
 
-        // Reference link: https://leetcode.com/problems/4sum/solutions/1904184/c-solution/
         public static IList<IList<int>> FourSum(int[] nums, int target)
         {
             Array.Sort(nums);
-            List<IList<int>> data = new List<IList<int>>();
-            int len = nums.Length - 1;
+            return kSum(nums, target, 0, 4);
+        }
 
-            //for first value
-            for (int i = 0; i < len - 2; i++)
+        public static List<IList<int>> kSum(int[] nums, long target, int start, int k) {
+            List<IList<int>> res = new List<IList<int>>();
+
+            // If we have run out of numbers to add, return res.
+            if (start == nums.Length)
             {
+                return res;
+            }
 
-                //first value can't repeat
-                if (i == 0 || nums[i] != nums[i - 1])
+            // There are k remaining values to add to the sum. The 
+            // average of these values is at least target / k.
+            long average_value = target / (long)k;
+
+            // We cannot obtain a sum of target if the smallest value
+            // in nums is greater than target / k or if the largest 
+            // value in nums is smaller than target / k.
+            if (nums[start] > average_value || average_value > nums[nums.Length - 1])
+            {
+                return res;
+            }
+
+            if (k == 2)
+            {
+                return twoSum(nums, target, start);
+            }
+
+            for (int i = start; i < nums.Length; ++i)
+            {
+                if (i == start || nums[i - 1] != nums[i])
                 {
 
-                    //same as 3-Sum solution
-                    //second value
-                    for (int j = i + 1; j < len - 1; j++)
-                    {
-
-                        //second value also can't repeat
-                        if (nums[j] != nums[j - 1] || j == i + 1)
-                        {
-
-                            //left and right position
-                            int left = j + 1, right = len;
-                            while (left < right)
-                            {
-                                int sum = nums[i] + nums[j] + nums[left] + nums[right];
-
-                                //check sum equals to target and 3rd value also can't repeat
-                                if (sum == target && (nums[left] != nums[left - 1] || left == j + 1))
-                                {
-                                    data.Add(new List<int> { nums[i], nums[j], nums[left], nums[right] });
-                                    left++;
-                                    right--;
-                                }
-                                else if (sum > target)
-                                {
-                                    right--;
-                                }
-                                else
-                                {
-                                    left++;
-                                }
-                            }
-                        }
+                    foreach (var subset in kSum(nums, target - nums[i], i + 1, k - 1)) {
+                        var tmp = new List<int> { nums[i] };
+                        tmp.AddRange(subset);
+                        res.Add(tmp);
                     }
                 }
             }
-            return data;
+
+            return res;
+        }
+
+        public static List<IList<int>> twoSum(int[] nums, long target, int start) {
+            List<IList<int>> res = new List<IList<int>>();
+            int lo = start;
+            int hi = nums.Length - 1;
+            while (lo < hi) {
+                int currSum = nums[lo] + nums[hi];
+                if (currSum < target || (lo > start && nums[lo] == nums[lo - 1]))
+                {
+                    ++lo;
+                }
+                else if (currSum > target || (hi < nums.Length - 1 && nums[hi] == nums[hi + 1]))
+                {
+                    --hi;
+                }
+                else {
+                    res.Add(new List<int>() { nums[lo++], nums[hi--] });
+                }
+            }
+
+            return res;
         }
     }
 }
---
+++ Leetcode_Console_App/Generate_Parentheses.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leetcode_Console_App
+{
+    public static class Generate_Parentheses
+    {
+        public static void Client() { }
+
+        // Reference link: https://leetcode.com/problems/generate-parentheses/editorial/
+
+        // Approach 3: Closure Number
+        public static IList<string> GenerateParenthesis(int n)
+        {
+            IList<string> ans = new List<string>();
+
+            if (n.Equals(0)) ans.Add("");
+            else {
+                for (int c = 0; c < n; c++) {
+                    foreach (var left in GenerateParenthesis(c)) {
+                        foreach (var right in GenerateParenthesis(n - 1 - c)) {
+                            ans.Add("(" + left + ")" + right);
+                        }
+                    }
+                }
+            }
+
+            return ans;
+        }
+
+        public static List<String> GenerateParenthesis2(int n)
+        {
+            List<string> ans = new List<string>();
+            backtrack(ans, new StringBuilder(), 0, 0, n);
+            return ans;
+        }
+
+        public static void backtrack(List<string> ans, StringBuilder cur, int open, int close, int max)
+        {
+            if (cur.Length == max * 2)
+            {
+                ans.Add(cur.ToString());
+                return;
+            }
+
+            if (open < max)
+            {
+                cur.Append("(");
+                backtrack(ans, cur, open + 1, close, max);
+                cur.Remove(cur.Length - 1, "(".Length);
+            }
+            if (close < open)
+            {
+                cur.Append(")");
+                backtrack(ans, cur, open, close + 1, max);
+                cur.Remove(cur.Length - 1, ")".Length);
+            }
+        }
+    }
+}
