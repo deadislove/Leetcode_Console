25b81a360062d1d32ce45706f68b46a6852e2817
DavidLin <taweilin7689@gmail.com>
2023-03-24 23:50:07 +0800

    Add 10 problems 3/24/2023

---
+++ Leetcode_Console_App/Find_First_and_Last_Position_of_Element_in_Sorted_Array.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Leetcode_Console_App
+{
+    internal static class Find_First_and_Last_Position_of_Element_in_Sorted_Array
+    {
+        public static void Client()
+        {
+            int[] inputArr = new int[] { 5, 7, 7, 8, 8, 10 };
+            var result = SearchRange(inputArr, 8);
+            foreach (var i in result) {
+                Console.WriteLine(i);
+            }
+        }
+
+        // Reference: https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/solutions/1970389/c-2-sol/
+        public static int[] SearchRange(int[] nums, int target)
+        {
+            int[] rets = { -1, -1 };
+            int i = 0, j = nums.Length - 1;
+            bool foundFirst = false, foundLast = false;
+
+            while (i <= j && !(foundFirst && foundLast))
+            {
+                if (!foundFirst)
+                {
+                    if (nums[i] == target)
+                    {
+                        rets[0] = i;
+                        foundFirst = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+                if (!foundLast)
+                {
+                    if (nums[j] == target)
+                    {
+                        rets[1] = j;
+                        foundLast = true;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+            }
+
+            return rets;
+        }
+    }
+}
---
+++ Leetcode_Console_App/Trapping_Rain_Water.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Leetcode_Console_App
+{
+    internal static class Trapping_Rain_Water
+    {
+        public static void Client() { }
+
+        public static int Trap(int[] height)
+        {
+            var leftarray = new int[height.Length];
+            var rightarray = new int[height.Length];
+
+            int left = 0;
+            int right = height.Length - 1;
+            int maxleft = 0;
+            int maxright = 0;
+            int water = 0;
+
+            while (right >= 0)
+            {
+                maxleft = Math.Max(maxleft, height[left]);
+                maxright = Math.Max(maxright, height[right]);
+                leftarray[left] = maxleft;
+                rightarray[right] = maxright;
+
+                left++;
+                right--;
+            }
+
+            for (int i = 1; i < height.Length - 1; i++)
+            {
+                var min = Math.Min(leftarray[i], rightarray[i]);
+                if (min >= height[i])
+                {
+                    water += min - height[i];
+                }
+            }
+
+            return water;
+        }
+
+        // Using 2 pointers
+        public static int Trap2(int[] height) {
+            int left = 0, right = height.Length - 1;
+            int ans = 0;
+            int left_max = 0, right_max = 0;
+            while (left < right)
+            {
+                if (height[left] < height[right])
+                {                     
+                    if (height[left] >= left_max)
+                        left_max = height[left];
+                    else
+                        ans += (left_max - height[left]);
+                    ++left;
+                }
+                else
+                {
+                    if (height[right] >= right_max)
+                        right_max = height[right];
+                    else
+                        ans += (right_max - height[right]);
+                    --right;
+                }
+            }
+            return ans;
+        }
+    }
+}
