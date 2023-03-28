1d1791cc735dc1d3a2da7fed4dc6be688a45f995
deadislove <taweilin7689@gmail.com>
2023-03-28 16:16:12 +0800

    add two cases 2023/3/28

---
+++ Leetcode_Console_App_dotnet6/Permutation_Sequence.cs
@@ -0,0 +1,42 @@
+﻿namespace Leetcode_Console_App_dotnet6
+{
+    internal static class Permutation_Sequence
+    {
+        public static void Client() { }
+
+        // Reference: https://leetcode.com/problems/permutation-sequence/solutions/2973999/js-py-java-c-spliting-in-windows-approach-backtracking/?orderBy=most_votes&languageTags=csharp
+        public static string GetPermutation(int n, int k) {
+            // create Set
+            List<int> set = new List<int>();
+            for (int i = 1; i <= n; i++) set.Add(i);
+            // find Kth permutation
+            return backtracking(n, k, set, "");
+        }
+
+        private static int factorial(int n) {
+            if (n == 1) return 1;
+            else return n * factorial(n - 1);
+        }
+
+        private static string backtracking(int N, int K, List<int> set, string permutation) {
+            // Base Case
+            if (N is 0) return permutation;
+
+            int fact = factorial(N),
+            combinations = fact / N, // size of window for each digit
+            index = 0; // index where K at windows
+
+            int[] windows = new int[N]; // max range of each window
+            for (int i = 0, c = combinations; i < N; i++, c += combinations)
+            {
+                windows[i] = c;
+                index += (K > c) ? 1 : 0;
+            }
+
+            permutation += set[index]; // add to permutation
+            set.RemoveAt(index); // remove from set
+            K -= (index > 0) ? windows[index - 1] : index; // reduce K
+            return backtracking(N - 1, K, set, permutation);
+        }
+    }
+}
---
+++ Leetcode_Console_App_dotnet6/Rotate_List.cs
@@ -0,0 +1,65 @@
+﻿namespace Leetcode_Console_App_dotnet6
+{
+    internal static class Rotate_List
+    {
+        public static void Client() { }
+
+        public class ListNode {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null) {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        /*
+         * step 1 = count number of elements linked
+         * step 2 = jump to first element of rotated
+         * step 3 = jump to last element of rotated
+         * step 4 = remove link of new_head from head at last element of list( reference of head )
+         * step 6 = link list( reference of new_head ) to head( original head except new_head link ) at last element of list( reference of new_head )
+         */
+
+        public static ListNode RotateRight(ListNode head, int k)
+        {
+            // step 1 = count number of elements linked
+            int length = getLength(head);
+
+            k %= (length > 0) ? length : 1; // eliminates multiple passes
+            if (k == 0 || head == null || head.next == null) return head;
+
+            // step 2 = jump to first element of rotated
+            ListNode new_head = head;
+            int jumps = length - k;
+            while (jumps-- > 0) new_head = new_head.next;
+
+            // step 3 = jump to last element of rotated
+            ListNode list = head;
+            jumps = length - k - 1;
+            while (jumps-- > 0) list = list.next;
+
+            // step 4 = remove link of new_head from head
+            // at last element of list( reference of head )
+            list.next = null;
+
+            // step 5 = move to last element of new_head
+            list = new_head;
+            while (list.next != null) list = list.next;
+
+            // step 6 = link list( reference of new_head ) to head( original head except new_head link )
+            // at last element of list( reference of new_head )
+            list.next = head;
+
+            return new_head;
+
+        }
+
+        private static int getLength(ListNode head)
+        {
+            int output = 0;
+            for (; head != null; head = head.next, output++) { };
+            return output;
+        }
+    }
+}
